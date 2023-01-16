d8dec3f27764c7a36aea023887cbb0b46d4a9e57
luisc8487 <Lf9904@gmail.com>
2023-01-16 06:41:56 +0000

    completed solution 1 and 2

--- problems/solutions.js
+++ problems/solutions.js
@@ -1,13 +1,27 @@
-
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4){
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.side4 = side4;
+  }
+  getPerimeter(){
+    return this.side1 + this.side2 + this.side3 + this.side4;
+  }
 }
 
-class Rectangle Quadrilateral{
-
+class Rectangle extends Quadrilateral {
+  constructor(side1, side2){
+    super(side1, side2)
+    this.side3 = side1;
+    this.side4 = side2;
+  }
+  getArea(){
+    return this.side1 * this.side2;
+  }
 }
 
-class Square Rectangle{
+class Square {
 
 }
 
